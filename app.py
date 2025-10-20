d35c67833a3dcce0f54e0423b9bdb2a8fe8fe61f
albionpepega-sketch <albionpepega@gmail.com>
2025-10-20 11:30:40 -0500

    app.py

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
-from flask import Flask
+from flask import Flask, jsonify
 from datetime import datetime
 import os
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return {"mensaje": "Microservicio de hora funcionando correctamente"}
-
-@app.route('/hora')
-def hora():
-    ahora = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    return {"hora_actual": ahora}
+def hora_actual():
+    hora = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    return jsonify({
+        "mensaje": "Microservicio de hora funcionando correctamente",
+        "hora_actual": hora
+    })
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
